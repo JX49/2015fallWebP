4bfd56bb3702ec7a6aae774e56fb10d7965bcc7e
JX49 <clossj1@hawkmail.newpaltz.edu>
2015-11-11 19:56:52 -0500

    Create _Template.php

---
+++ php/Views/shared/_Template.php
@@ -0,0 +1,45 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Inforun</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  </head>
+  <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+      
+    <div class="container">
+        <h1>Inforun</h1>
+              <nav class="navbar navbar-default navbar-inverse">
+        <div class="container-fluid">
+         
+
+        </div><!--/.container-fluid -->
+      </nav>
+
+            <?php include __DIR__ . "/../$view"; ?>
+    </div>
+ 
+ 
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script type="text/javascript">
+      (function($){
+        $(function(){
+          
+          
+             $("body").css("background-color", "#0099CC");
+            
+        });
+      })(jQuery);
+    </script>
+  </body>
+</html>
