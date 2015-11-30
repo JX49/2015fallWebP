b7b053e433d3e82527a496edddada143b5b6510f
JX49 <clossj1@hawkmail.newpaltz.edu>
2015-11-30 09:35:54 -0500

    Update express.js

--- node/spa/express.js
+++ node/spa/express.js
@@ -3,18 +3,85 @@ var express = require('express'),
 var bodyParser = require('body-parser');
 
 var food = require("./model/food");
+var person = require("./model/person");
+var exercise = require("./model/exercise");
 
 
-var data = [{id: 0, Name: "Billy"}, {id: 1, Name: "Krackadilla"}, {id: 2, Name: "Morgul"}];
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(bodyParser.json());
+
+
+
+
+
+app.get("/exercise", function(req, res){
+  
+  exercise.get(null, function(err, rows){
+    if(err){
+      res.status(500).send(err);
+      return;
+    }
+    res.send(rows);
+  })
+    
+})
+.get("/exercise/:id", function(req, res){
+  
+  exercise.get(req.params.id, function(err, rows){
+       if(err){
+      res.status(500).send(err);
+      return;
+    }
+    res.send(rows[0]);
+  })
+  
+})
+.post("/exercise", function(req, res){
+  var errors = exercise.validate(req.body);
+  if(errors){
+    res.status(500).send(errors);
+    return;
+  }
+  exercise.save(req.body, function(err, row){
+     if(err){
+      res.status(500).send(err);
+      return;
+    }
+    res.send(row);
+  })
+  
+})
+.delete("/exercise/:id", function(req, res){
+  
+  exercise.delete(req.params.id, function(err, rows){
+      if(err){
+        res.status(500).send(err);
+      }else{
+        res.send(req.params.id);
+      }
+  })
+  
+})
+
+
+
+
+
+
+
 
 app.get("/food", function(req, res){
   
   food.get(null, function(err, rows){
+    if(err){
+      res.status(500).send(err);
+      return;
+    }
     res.send(rows);
   })
     
@@ -22,6 +89,10 @@ app.get("/food", function(req, res){
 .get("/food/:id", function(req, res){
   
   food.get(req.params.id, function(err, rows){
+       if(err){
+      res.status(500).send(err);
+      return;
+    }
     res.send(rows[0]);
   })
   
@@ -33,10 +104,80 @@ app.get("/food", function(req, res){
     return;
   }
   food.save(req.body, function(err, row){
+     if(err){
+      res.status(500).send(err);
+      return;
+    }
     res.send(row);
   })
   
 })
+.delete("/food/:id", function(req, res){
+  
+  food.delete(req.params.id, function(err, rows){
+      if(err){
+        res.status(500).send(err);
+      }else{
+        res.send(req.params.id);
+      }
+  })
+  
+})
+
+
+
+
+
+
+app.get("/person", function(req, res){
+  
+  person.get(null, function(err, rows){
+    if(err){
+      res.status(500).send(err);
+      return;
+    }
+    res.send(rows);
+  })
+    
+})
+.get("/person/:id", function(req, res){
+  
+  person.get(req.params.id, function(err, rows){
+       if(err){
+      res.status(500).send(err);
+      return;
+    }
+    res.send(rows[0]);
+  })
+  
+})
+.post("/person", function(req, res){
+  var errors = person.validate(req.body);
+  if(errors){
+    res.status(500).send(errors);
+    return;
+  }
+  person.save(req.body, function(err, row){
+     if(err){
+      res.status(500).send(err);
+      return;
+    }
+    res.send(row);
+  })
+  
+})
+.delete("/person/:id", function(req, res){
+  
+  person.delete(req.params.id, function(err, rows){
+      if(err){
+        res.status(500).send(err);
+      }else{
+        res.send(req.params.id);
+      }
+  })
+  
+})
+
 
 
 app.listen(process.env.PORT);
