0b6e876ae79cb81ce1fccafe22fdf27febe14213
JX49 <clossj1@hawkmail.newpaltz.edu>
2015-12-13 14:15:18 -0500

    Delete email.js

--- fin/spa/controllers/email.js
+++
@@ -1,9 +0,0 @@
-     angular.module('app')
-        .controller('email', function($http, alert, panel){
-            var self = this;
-            
-            $http.get("/journal")
-            .success(function(data){
-                self.rows = data;
-            });
-        });
